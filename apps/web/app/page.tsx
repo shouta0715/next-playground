07a1ccd2451c0e0e828b4444a4cb68dd07185dfd
shouta0715 <kshouta0715@gmail.com>
2025-04-15 21:07:58 +0900

    add shadcn

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
-  return <div className="text-red-500">a</div>;
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 }
